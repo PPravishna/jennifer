a22d2143ec9d460ee03267af22f0a0193d364c5e
PPravishna <ppravishna2002@gmail.com>
2023-08-28 22:55:18 +0530

    java-introduction

---
+++ fullstack/backend/languages/Java_lessons/src/basic_java_lessons/Fan.java
@@ -0,0 +1,23 @@
+package basic_java_lessons;
+
+public class Fan {
+String Wing;
+	public static void main(String[] args) {
+		System.out.println("iam running");
+		withWing();
+	Fan memory=new Fan();
+	memory.color();
+			
+
+	}
+	
+	static void withWing() {
+	   System.out.println("I rotate");
+		
+	}
+	void color() {
+		System.out.println("I give a look");
+			
+		}
+
+}
---
+++ fullstack/backend/languages/Java_lessons/src/basic_java_lessons/IntroWithVSCode.java
@@ -0,0 +1,7 @@
+package basic_java_lessons;
+
+public class IntroWithVSCode{
+    public static void main(String[] args){
+        System.out.println("iam vscode");
+    }
+}
