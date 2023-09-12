d97afb3636f319d5cd7ee0b02e671a2e79117297
PPravishna <ppravishna2002@gmail.com>
2023-09-12 14:30:49 +0530

    first commit

---
+++ src/components/Charts/Likelihood.tsx
@@ -0,0 +1,14 @@
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+const data1 = [{end_year: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+
+export const Likelihood:React.FC<{data:any}>=({data})=>{
+    return <>
+     <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <Line type="monotone" dataKey="intensity" stroke="#8884d8" />
+    <CartesianGrid stroke="#ccc"  />
+    <XAxis dataKey="end_year" />
+    <YAxis dataKey="likelihood" />
+    <Tooltip />
+  </LineChart>
+    </>
+}
