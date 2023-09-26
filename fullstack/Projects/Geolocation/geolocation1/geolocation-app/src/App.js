96d832150e6aad08f4e0b2c49ba6cae64499853c
PPravishna <ppravishna2002@gmail.com>
2023-09-26 11:45:17 +0530

    geolocation

---
+++ fullstack/Projects/Geolocation/geolocation1/geolocation-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import GeolocationCapture from "./components/GeolocationCapture";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Geolocation Capture</h1>
+        <GeolocationCapture />
+      </header>
+    </div>
+  );
+}
+
+export default App;
